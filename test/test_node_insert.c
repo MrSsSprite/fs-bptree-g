12a46ae1fb5a3de753f6761fd4a01f7af89fded0
Archeus Lam <mrsssprite@gmail.com>
2026-03-08 21:05:35 +0800

    test: complete obj lifecycle for 1st unit test

--- test/test_node_insert.c
+++ test/test_node_insert.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <inttypes.h>
 
 #include "../src/bptr_internal.h"
 #include "../bptree.h"
@@ -13,14 +14,21 @@ int cmp_i(const void *lhs, const void *rhs)
 void test_insert_middle(void)
 {
    int val;
-   struct bptr *bptr = bptr_init("test_node_insert.bptr", 1, 512,
+   static const char *f_nm = "test_node_insert.bptr";
+   struct bptr *bptr = bptr_init(f_nm, 1, 512,
                                  sizeof(int), sizeof(int), cmp_i);
-   struct bptr_node *node = bptr_node_new(bptr, 1, 0);
-
    assert(bptr);
+   struct bptr_node *node = bptr_node_new(bptr, 1, 0);
    assert(node);
-   val = 0;
+
+   val = 123;
+   printf("Key count: %" PRIu32 "\n", node->key_count);
    _node_key_insert(bptr, node, &val, 0);
+   assert(((int*)node->keys)[0] == 123);
+
+   bptr_node_free(node);
+   assert(bptr_destroy(bptr) == 0);
+   assert(remove(f_nm) == 0);
 }
 
 
