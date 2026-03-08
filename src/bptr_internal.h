152ecf4c90616d07407fd9328e8336250b7d6828
Archeus Lam <mrsssprite@gmail.com>
2026-03-08 16:53:50 +0800

    feat(*): add unreachable macro

--- src/bptr_internal.h
+++ src/bptr_internal.h
@@ -4,6 +4,7 @@
 /*----------------------------- Public Includes ------------------------------*/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "../bptree.h"
 /*--------------------------- Public Includes END ----------------------------*/
 
@@ -26,6 +27,23 @@ typedef uint_fast64_t bptr_node_t;
 
 /*------------------------------ Public Macros -------------------------------*/
 #define BPTR_PTR_SIZE (self->is_lite ? BPTR_LITE_PTR_BYTE : BPTR_NORM_PTR_BYTE)
+
+#if defined(__has_builtin)
+    #if __has_builtin(__builtin_unreachable)
+        #define BPTR_UNREACHABLE() __builtin_unreachable()
+    #else
+        #define BPTR_UNREACHABLE() exit(BPTR_E_UNREACHABLE)
+    #endif
+#elif defined(_MSC_VER)
+    /* MSVC equivalent */
+    #define BPTR_UNREACHABLE() __assume(0)
+#elif defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5))
+    /* Older GCC (4.5+) doesn't always have __has_builtin but has the intrinsic */
+    #define BPTR_UNREACHABLE() __builtin_unreachable()
+#else
+    /* Fallback for everyone else */
+    #define BPTR_UNREACHABLE() exit(BPTR_E_UNREACHABLE)
+#endif
 /*---------------------------- Public Macros END -----------------------------*/
 
 /*----------------------------- Public Structs ------------------------------*/
