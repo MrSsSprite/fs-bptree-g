b3a5cd088060cbd80e151e5de2c01afe1e645fff
Archeus Lam <mrsssprite@gmail.com>
2026-03-08 21:18:54 +0800

    test: 1st test unit work; add print_node_keys fn

--- test/test_node_insert.c
+++ test/test_node_insert.c
@@ -11,6 +11,16 @@ int cmp_i(const void *lhs, const void *rhs)
 { return *(const int*)lhs - *(const int *)rhs; }
 
 
+void print_node_keys(struct bptr_node *node)
+{
+   const int *arr = node->keys;
+   putchar('[');
+   for (uint_fast32_t i = 0; i < node->key_count; i++)
+      printf("%d, ", arr[i]);
+   puts("]");
+}
+
+
 void test_insert_middle(void)
 {
    int val;
@@ -22,9 +32,27 @@ void test_insert_middle(void)
    assert(node);
 
    val = 123;
-   printf("Key count: %" PRIu32 "\n", node->key_count);
+   printf("Key count: %" PRIu32 ", Key max: %" PRIuFAST16 "\n", node->key_count, bptr->node_boundry.leaf.up - 1);
    _node_key_insert(bptr, node, &val, 0);
    assert(((int*)node->keys)[0] == 123);
+   node->key_count++;
+
+   val = 789;
+   _node_key_insert(bptr, node, &val, 1);
+   assert(((int*)node->keys)[1] == 789);
+   node->key_count++;
+
+   val = 321;
+   _node_key_insert(bptr, node, &val, 0);
+   assert(((int*)node->keys)[0] == 321);
+   node->key_count++;
+
+   val = 456;
+   _node_key_insert(bptr, node, &val, 2);
+   assert(((int*)node->keys)[2] == 456);
+   node->key_count++;
+
+   print_node_keys(node);
 
    bptr_node_free(node);
    assert(bptr_destroy(bptr) == 0);
