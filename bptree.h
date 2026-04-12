9dd470a1137a2915a07b9770b3487ae5c890154f
Archeus Lam <mrsssprite@gmail.com>
2026-04-12 19:35:13 +0800

    feat(E const): add E_FCLOSE
    BPTR_E_FCLOSE is added in addition to BPTR_E_FACCESS because after
    fclose, it is not valid to access the file, while access failure usually
    does not invalidate the file.

--- bptree.h
+++ bptree.h
@@ -13,6 +13,7 @@
 // Sytem & Program-side Errors (Positive)
 #define BPTR_E_OOM         (1u)      // OOM: Out Of Memory
 #define BPTR_E_FACCESS     (2u)      // File Access Error
+#define BPTR_E_FCLOSE      (3u)      // Failed at fclose
 #define BPTR_E_UNREACHABLE (0xDEADu) // Unreachable (57005 dec.)
 #define BPTR_E_TODO        (0xBEEFu) // Not yet implemented (48879 dec.)
 // User & Argument Errors (Negative)
