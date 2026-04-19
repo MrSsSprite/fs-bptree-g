233ee3bc121b5ae56219af7bcbb2d116fca1b0ab
Archeus Lam <mrsssprite@gmail.com>
2026-04-19 13:43:54 +0800

    fix(io/fload): correct unmarshal to match layout
    Correct the deserialization in `bptr_io_fload` to match the binary
    layout of metadata of the tree as defined in
    docs/header_bin_layout.md.

--- src/bptr_io.c
+++ src/bptr_io.c
@@ -159,12 +159,10 @@ int bptr_io_fload(struct bptr *self, const char *filename)
 
    /* load metadata in header block into handler */
    memit = self->fbuf + 12;
-   self->node_size = *(uint32_t*)memit;
-   memit += 4;
    self->key_size = *(uint16_t*)memit;
-   memit += 1;
+   memit += 2;
    self->value_size = *(uint16_t*)memit;
-   memit += 3; // +1 for padding
+   memit += 2;
    self->record_cnt = *(uint64_t*)memit;
    memit += 8;
    self->height = *(uint32_t*)memit;
