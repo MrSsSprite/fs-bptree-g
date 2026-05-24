ef9d4b9ea729ac3713cd8b4ee4d5f695361e1003
Ryan Lam <mrsssprite@gmail.com>
2026-05-24 21:34:14 +0800

    feat(*): add set_errno macro

--- src/bptr_utils.h
+++ src/bptr_utils.h
@@ -3,6 +3,9 @@
 
 /*------------------------------ Public Macros -------------------------------*/
 #define CEIL_DIV(num, dno) (((num) + (dno) - 1) / (dno))
+
+#define _bptr_set_errno(err_code) \
+   if (!err_set) { bptr_errno = err_code; err_set = 1; }
 /*---------------------------- Public Macros END -----------------------------*/
 
 #endif
