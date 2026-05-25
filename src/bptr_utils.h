a064ac159da94d506ee5459e1cd128b7b23a4853
Ryan Lam <mrsssprite@gmail.com>
2026-05-25 14:37:02 +0800

    feat(utils): add set err macros

--- src/bptr_utils.h
+++ src/bptr_utils.h
@@ -4,8 +4,13 @@
 /*------------------------------ Public Macros -------------------------------*/
 #define CEIL_DIV(num, dno) (((num) + (dno) - 1) / (dno))
 
-#define _bptr_set_errno(err_code) \
-   if (!err_set) { bptr_errno = err_code; err_set = 1; }
+#define _set_errno_(has_set_e, e_code) do \
+{ if (!(has_set_e)) { bptr_errno = e_code; has_set_e = 1; } } while (0)
+#define _set_errno(err_code) _set_errno_(has_set_err, err_code)
+
+#define _set_err_code_(has_set_e, e_code_var, e_code) do \
+{ if (!(has_set_e)) { (e_code_var) = (e_code); (has_set_e) = 1; } } while (0)
+#define _set_err_code(e_code) _set_err_code_(has_set_err, err_code, e_code)
 /*---------------------------- Public Macros END -----------------------------*/
 
 #endif
