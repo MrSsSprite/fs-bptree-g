bc13574701aa6331c0fa5c05b6c5c12a50150443
Archeus Lam <mrsssprite@gmail.com>
2026-06-22 17:59:14 +0800

    fix(io): revert BPTR_STATIC inline
    `static inline' was changed to `BPTR_STATIC inline' in previous mass
    replacement. Revert it back to `static inline' to avoid link error.
    
    Also, these functions are strictly static. For test units, the
    definitions should be copied entirely.

--- src/bptr_io.c
+++ src/bptr_io.c
@@ -42,7 +42,7 @@
  *
  * @param[in,out] self  bptr obj. Only @c fbuf member is modified.
  */
-BPTR_STATIC inline
+static inline
 void bptr_header_marshal(struct bptr *self);
 /**
  * @brief   Flush metadata from fbuf to the file
@@ -59,7 +59,7 @@ void bptr_header_marshal(struct bptr *self);
  *
  * @see  bptr_header_marshal
  */
-BPTR_STATIC inline
+static inline
 int _flush_to_header(struct bptr *self);
 /**
  * @brief   Write bptr obj. content to file header.
@@ -73,7 +73,7 @@ int _flush_to_header(struct bptr *self);
  * @see  _flush_to_header
  * @see  bptr_header_marshal
  */
-BPTR_STATIC inline
+static inline
 int _header_fwrite(struct bptr *self)
 { bptr_header_marshal(self); return _flush_to_header(self); }
 /*}---------------------- Private Function Declaration -----------------------*/
@@ -262,7 +262,7 @@ INVALID_INPUT_ERR:   _set_err_code(BPTR_E_FN_INPUT);
 /*--------------------------- Public Functions END ---------------------------*/
 
 /*{--------------------------- Private Functions -----------------------------*/
-BPTR_STATIC inline
+static inline
 void bptr_header_marshal(struct bptr *self)
 {
    char *memit = self->fbuf;
@@ -294,7 +294,7 @@ void bptr_header_marshal(struct bptr *self)
 
 
 // Flush the Buffer to the file
-BPTR_STATIC inline
+static inline
 int _flush_to_header(struct bptr *self)
 {
    if (fseek64(self->file, 0, SEEK_SET))
